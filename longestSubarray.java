70d665898a31b9ff83905c449d0ceb1ed715eb59
ChaiatnayaJadhav9322 <168905651+ChaitanyaJadhav9322@users.noreply.github.com>
2025-06-18 13:36:20 +0530

    Create longestSubarray.java

---
+++ longestSubarray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+       int maxLen = 0 ;
+       int sum = 0;
+    
+       Map <Integer , Integer > map = new HashMap<>();
+
+       for(int i = 0 ; i < nums.length ; i++)
+       {
+        sum+=nums[i];
+
+        if(sum == k)
+        {
+            maxLen = i + 1 ;
+        }
+
+        if(map.containsKey(sum - k))
+        {
+            maxLen = Math.max(maxLen ,i - map.get(sum - k));
+        }
+
+
+        if(!map.containsKey(sum))
+        {
+            map.put(sum , i );
+        }
+       }
+
+       return maxLen;
+    }
+}
